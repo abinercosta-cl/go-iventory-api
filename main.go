0ba5c9b65c682667700d8a4a07ac654cd39aa486
abinercosta-cl <abinerclcosta@gmail.com>
2025-07-16 14:10:01 -0300

    feat:integra o framework Gin e define rotas de produtos

--- main.go
+++ main.go
@@ -6,16 +6,35 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Funçoes Handler (Controladores)
+
+func createProduct(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "Endpoint para criar Produtos!"})
+}
+
+func getProducts(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "Endpoint para criar Produtos!"})
+}
+
+func getProductsByID(c *gin.Context) {
+	//Pegando o ID do produto da rota
+	id := c.Param("id")
+	c.JSON(http.StatusOK, gin.H{"message": "Endpoint para obter o produto com ID: " + id})
+}
+
+// Função Principal do servidor
 func main() {
 	//cria um roteador padrão com gin
 	router := gin.Default()
 
-	//Primeiro endpoint Gin
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "API de estoque com Gin Rodando!",
-		})
-	})
+	//Agrupando as Rotas de produtos
+	productRoutes := router.Group("/products")
+	{
+		productRoutes.POST("/", createProduct)
+		productRoutes.GET("/", getProducts)
+		productRoutes.GET("/:id", getProductsByID)
+
+	}
 
 	//Roda o servidor na porta 8080
 	router.Run(":8080")
